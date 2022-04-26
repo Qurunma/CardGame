540172c0da212ca09bbc4f54ce813a7a744a090f
Qurunma <yaestgrut75@gmail.com>
2022-04-26 21:07:26 +0300

    All ready

--- CheckerScore.php
+++ CheckerScore.php
@@ -7,8 +7,11 @@ function reader()
   if ($file) {
     while (!feof($file)) {
       $buffer = fgets($file);
-      if ($buffer !== "\n") {
-        $scoresFromFile[] = $buffer;
+      if ($buffer !== "\n" && $buffer != " ") {
+        $buffer = explode(" ", $buffer);
+        if ($buffer[0] != ""){
+          $scoresFromFile[] = ["name"=>$buffer[0], "score"=>$buffer[1]];
+        }
       }
     }
   }
@@ -20,27 +23,90 @@ function writer($scoresFromFile)
 {
   $file = fopen("highScores.txt", "w");
   for ($i = 0; $i < count($scoresFromFile); $i++) {
+    $buffer = $scoresFromFile[$i];
+    var_dump($buffer);
     fwrite($file, "\n");
-    fwrite($file, "$scoresFromFile[$i]");
+    fwrite($file, "$buffer[name] ");
+    fwrite($file, "$buffer[score]");
     fwrite($file, "\n");
   }
   fclose($file);
 }
 
 function checker($scoreForCheck, $scoresFromFile)
-{if (count($scoresFromFile) < 10) {
-    $scoresFromFile[] = $scoreForCheck;
-    rsort($scoresFromFile);
-    writer($scoresFromFile);
-  }
-  else if (count($scoresFromFile) == 10){
-    rsort($scoresFromFile);
-    if ($scoreForCheck > $scoresFromFile[count($scoresFromFile)-1]) {
-      $scoresFromFile[count($scoresFromFile)-1] = $scoreForCheck;
+{
+  $scoreForCheck = (int)$scoreForCheck;
+  var_dump($scoreForCheck);
+  echo "<br>";
+  if (count($scoresFromFile) == 10)
+  {
+    for ($i = 0; $i < count($scoresFromFile); $i++) {
+      if ($scoreForCheck < (int)$scoresFromFile[$i]["score"]) {
+        continue;
+      }
+      else {
+        if ($_COOKIE["login"] != ""){
+          // echo "fff";
+          $scoresFromFile = array_merge(array_slice($scoresFromFile, 0, $i) || [], [["name"=>"$_COOKIE[login]:", "score"=>$scoreForCheck]], array_slice($scoresFromFile, 0, count($scoresFromFile)-$i) || []);
+        }
+        else {
+          echo "fff";
+
+          $randNum = rand(0, 1000000);
+          $scoresFromFile = array_merge(array_slice($scoresFromFile, 0, $i), [["name"=>"Guest$randNum:", "score"=>"$scoreForCheck"]], array_slice($scoresFromFile, $i+1, count($scoresFromFile)-$i));
+        }
+        if (count($scoresFromFile) > 10){
+          array_pop($scoresFromFile);
+        }
+        break;
+      }
     }
-    rsort($scoresFromFile);
-    writer($scoresFromFile);
   }
+  else if (count($scoresFromFile) != 0) {
+    $inp = false; 
+    for ($i = 0; $i < count($scoresFromFile); $i++) {
+      if ($scoreForCheck < (int)$scoresFromFile[$i]["score"]) {
+        continue;
+      }
+      else {
+        if ($_COOKIE["login"] != ""){
+          global $inp;
+          $inp = true;
+          $scoresFromFile = array_merge(array_slice($scoresFromFile, 0, $i), [["name"=>"$_COOKIE[login]:", "score"=>$scoreForCheck]], array_slice($scoresFromFile, 0, count($scoresFromFile)-$i));
+        }
+        else {
+          global $inp;
+          $inp = true;
+          echo "aaa";
+          $randNum = rand(0, 1000000);
+          $scoresFromFile = array_merge(array_slice($scoresFromFile, 0, $i), [["name"=>"Guest$randNum:", "score"=>"$scoreForCheck"]], array_slice($scoresFromFile, $i));
+        }
+      }
+      break;
+    }
+    if (!$inp){
+      if ($_COOKIE["login"] != ""){
+          $scoresFromFile[] = ["name"=>"$_COOKIE[login]:", "score"=>$scoreForCheck];
+        }
+        else {
+          echo "eee";
+          $randNum = rand(0, 1000000);
+          $scoresFromFile[] =["name"=>"Guest$randNum:", "score"=>"$scoreForCheck"];
+        }
+    }
+  }
+  else {
+    if ($_COOKIE["login"] != ""){
+        $scoresFromFile[] = ["name"=>"$_COOKIE[login]:", "score"=>$scoreForCheck];
+    }
+    else {
+      echo "xxd";
+      $randNum = rand(0, 1000000);
+      $scoresFromFile[] = ["name"=>"Guest$randNum:", "score"=>$scoreForCheck];
+    }
+  }
+
+  writer($scoresFromFile);
 }
 
 $scoresFromFile = reader();
